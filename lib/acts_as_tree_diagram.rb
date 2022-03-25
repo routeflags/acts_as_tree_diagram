bc60e9083e24f80d33ee081ac184eb939654793b
smapira <smapira@routeflags.com>
2022-03-26 02:19:56 +0900

    Exec rails plugin new acts_as_tree_diagram

---
+++ lib/acts_as_tree_diagram.rb
@@ -0,0 +1,6 @@
+require "acts_as_tree_diagram/version"
+require "acts_as_tree_diagram/railtie"
+
+module ActsAsTreeDiagram
+  # Your code goes here...
+end
---
+++ lib/acts_as_tree_diagram/railtie.rb
@@ -0,0 +1,4 @@
+module ActsAsTreeDiagram
+  class Railtie < ::Rails::Railtie
+  end
+end
