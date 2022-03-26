cdaab426cb621556dc1f8292e387efd9989205e4
smapira <smapira@routeflags.com>
2022-03-26 14:59:16 +0900

    wip

--- lib/acts_as_tree_diagram.rb
+++ lib/acts_as_tree_diagram.rb
@@ -1,34 +1,31 @@
 require "acts_as_tree_diagram/version"
 require "acts_as_tree_diagram/railtie"
-require "acts_as_tree_diagram/core_ext"
 require "action_view"
 
 module ActsAsTreeDiagram
 	module ViewDiagram
 		def draw_diagram(options = {}, &block)
 			where = options.fetch :where, {}
-			# where(where).map{|x| draw_tree(x) }.join("\n")
-			draw_tree(find(1))
+			where(where).map{|x| draw_tree(x, options) }.join("\n")
 		end
 
-		def draw_tree(node)
+		def draw_tree(node, options = {})
 			html = ''
 			node.children.each do |child|
-				# html = html.concat "<li><code>#{generate_tree_url(child, child.name)}</code>"
-				html = html.concat "<li><code>#{child.name}</code>"
-				html.concat "<ul>#{draw_tree(child)}</ul>" if child.children.any?
+				path = options.fetch :path, ''
+				if path.blank?
+					html = html.concat "<li><code>#{child.name}</code>"
+				else
+					html = html.concat "<li><code>#{generate_tree_url(path, child, child.name)}</code>"
+				end
+				html.concat "<ul>#{draw_tree(child, options)}</ul>" if child.children.any?
 				html.concat '</li>'
 			end
 			html.html_safe
 		end
 
-		def generate_tree_url(node, label)
-			# include ActionView::Helpers::UrlHelper
-			# link_to label, animal_path(node)
-		end
-
-		def animal_path(animal)
-			"/animals/#{animal.id}"
+		def generate_tree_url(path, node, label)
+			"<a href='#{path}/#{node.id}'>#{label}</a>"
 		end
 
 	end
